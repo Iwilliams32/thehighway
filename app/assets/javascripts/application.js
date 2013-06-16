13728fc1b9409894d57ac624ee266ab9efdbbd1b
charlest100 <charlesthomas100@gmail.com>
2013-06-16 19:32:18 -0400

    added: ember.js & bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require thehighway
+
+// for more details see: http://emberjs.com/guides/application/
+Thehighway = Ember.Application.create();
+
 //= require_tree .
