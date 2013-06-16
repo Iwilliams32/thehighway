9ab19b9dec19c7ef818e9fd62eb3ebdafdfed32b
charlest100 <charlesthomas100@gmail.com>
2013-06-16 19:10:54 -0400

    initial commit: rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thehighway::Application.config.secret_token = '3f6d0729e0515e635632bccaa999388253c9e86a7a760bc393271a688a5d8b59f2e7a62c4c9949fd5591dd38de4faad9174e81659b62de0cff816ee838ac9bb2'
