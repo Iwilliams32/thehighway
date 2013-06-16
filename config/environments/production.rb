b637290182e66601e8fb7d6962468fd2f2065eda
charlest100 <charlesthomas100@gmail.com>
2013-06-16 19:30:10 -0400

    added: ember to dev & pro config env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Thehighway::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Ember.js
+  config.ember.variant = :production
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
